523f02d56aa04a5eb20fb1e6468b9f345a546506
samarth <samarth@payatu.io>
2022-01-10 16:13:06 +0530

    week2, t1 complete

--- week2/t1.rb
+++ week2/t1.rb
@@ -1,22 +1,38 @@
 require 'net/http'
+require 'json'
+require 'uri'
+
+def findLocation(location)
+    params = {:query => location}
+    uri = URI("https://www.metaweather.com/api/location/search/")
+    uri.query = URI.encode_www_form(params)
+    json = Net::HTTP.get(uri)
+    res = JSON.parse(json)
+
+    return res[0]['woeid']
+end
+
+def findWeather(id)
+    uri = URI("https://www.metaweather.com/api/location/#{id}/")
+    json = Net::HTTP.get(uri)
+    res = JSON.parse(json)
+    return res
+end
 
 
-codes = Hash{ "sn" => "Snow",
-        "sl" => "Sleet",
-        "h" => "Hail",
-        "t" => "Thunderstorm",
-        "hr" => "Heavy_Rain",
-        "lr" => "Light_Rain",
-        "s" => "Showers",
-        "hc" => "Heavy_Cloud",
-        "lc" => "Light_Cloud",
-        "c" => "Clear",
-    }
 
-def findLocation(name)
-    url = URI.parse('https://www.metaweather.com/api/location/search/?query=#{name}')
-    req = Net::HTTP.get(url)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    puts res.body
+puts 'enter location name'
+location = gets.chomp
+
+id = findLocation(location)
+
+res = findWeather(id)
+
+state    = res['consolidated_weather'][0]['weather_state_name']
+min_t    = res['consolidated_weather'][0]['min_temp']
+max_t    = res['consolidated_weather'][0]['max_temp']
+humidity = res['consolidated_weather'][0]['humidity']
+
+puts "#{location}'s state: #{state} minimum temp: #{min_t}, maximum temp: #{max_t}, humidity: #{humidity}"
+
+
