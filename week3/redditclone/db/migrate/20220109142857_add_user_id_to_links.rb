b01cbf447a9c9813f14ab44f0b56771e7ee9b9da
samarth <samarth@payatu.io>
2022-01-12 03:02:49 +0530

    cleaned week3

--- week3/redditclone/db/migrate/20220109142857_add_user_id_to_links.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToLinks < ActiveRecord::Migration[7.0]
-  def change
-    add_column :links, :user_id, :integer
-    add_index :links, :user_id
-  end
-end
--- week3/redditclone/db/migrate/20220109190749_change_links_scaffold_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class ChangeLinksScaffoldToPosts < ActiveRecord::Migration[7.0]
-  def change
-    change_table :links do |t|
-      t.remove :url
-      t.text :post
-      t.integer :vote
-    end
-  end
-end
